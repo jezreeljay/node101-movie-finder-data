168bb29833cf159b6608546b5ce4ef1aeea6cb19
jezreeljay <iam@jezreeljay.com>
2018-11-08 13:04:47 -0800

    Adds conditions to end request if no params are passed.

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const axios = require('axios');
 let cache = {};
 
-axios.defaults.baseURL = 'http://localhost:3000';
+// axios.defaults.baseURL = 'http://localhost:3000';
 // axios.defaults.headers = {'Content-Type': 'application/json'};
 app.use(morgan('dev'));
 
@@ -18,35 +18,42 @@ switch (true) {
             .then((data) => {
             cache[movieID]=data.data;
             //Send API response
-            res.send(cache[movieID]);
+            res.send(cache[movieID]).end();
             })
             .catch((err) => {
-            // console.log(err.res);
+            console.log(err.res);
             })
         //Send cached data            
         if (cache[movieID] !== undefined) {
-            res.send(cache[movieID]);
+            res.send(cache[movieID]).end();
         } else {
             return undefined;
         }
+    break;
     case (movieTitle !== undefined):
         axios.get(encodeURI(`http://www.omdbapi.com/?t=${movieTitle}&apikey=8730e0e`))
             .then((data) => {
             cache[movieTitle]=data.data;
             //Send API response
-            res.send(cache[movieTitle]);
+            res.send(cache[movieTitle]).end();
             })
             .catch((err) => {
-            // console.log(err.res); 
+            console.log(err.res); 
             })
         //Send cached data            
         if (cache[movieTitle] !== undefined) {
-            res.send(cache[movieTitle]);
+            res.send(cache[movieTitle]).end();
         } else {
             return undefined;
         }
+    default:
+    res.end('No movie to fetch!');
     }
 });
+
+app.use('/', (req, res) => {
+    res.end('Nada!');
+})
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
